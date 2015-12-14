0c9dcaca3c61f0ca3b5a0e8695d78e4491b39006
Jorn Wijnands <jwijnands@ebay.com>
2015-12-14 18:05:57 +0100

    fix monitoring messages and thresholds

--- znappy/monitor/snapshot.py
+++ znappy/monitor/snapshot.py
@@ -7,16 +7,20 @@ import time
 
 def snapshot_age(znappy, *args, **kwargs):
     snapshots = sorted(znappy.host.snapshots.values(), key=lambda s: s.time, reverse=True)
+
+    if len(snapshots) == 0:
+        return False, (2, "CRITICAL: No snapshots found!")
+
     last_snapshot = snapshots[0]
 
     lag = (int(time.time()) - znappy.config.get('snapshot', {}).get('min_age', 3600)) - last_snapshot.time
 
-    if lag < 5:
-        return True, (0, "OK: snapshot age is less then 10 seconds")
+    if lag < 10:
+        return True, (0, "OK: last snapshot age is less than 10 seconds")
     elif lag < 30:
-        return False, (1, "WARNING: snapshot age is between 10 and 30 seconds")
+        return False, (1, "WARNING: last snapshot age is between 10 and 30 seconds")
     else:
-        return False, (2, "CRITAL: snapshot lag is more then 30 seconds!")
+        return False, (2, "CRITICAL: last snapshot lag is more than 30 seconds!")
 
 
 def snapshot_count(znappy, *args, **kwargs):
@@ -24,9 +28,9 @@ def snapshot_count(znappy, *args, **kwargs):
     rotate =  znappy.config.get("snapshot", {}).get("rotate", 12)
 
     if count < rotate:
-        return False, (1, "WARNING: snapshot count is lower then the rotation ({}/{})".format(count, rotate))
+        return False, (1, "WARNING: snapshot count is lower than the rotation ({}/{})".format(count, rotate))
     if count > (rotate + 1):
-        return False, (2, "CRITAL: snapshot count is higher then rotation! ({}/{})".format(count, rotate))
+        return False, (2, "CRITICAL: snapshot count is higher than rotation! ({}/{})".format(count, rotate))
 
     return True, (0, "OK: snapshot count is ok")
 
