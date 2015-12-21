574787f6043d943f19aa06502829c30895ef2843
Yorick Gersie <ygersie@ebay.com>
2015-12-21 12:29:06 +0100

    mysql: failover after restoring snapshots
      - Start MySQL without starting the Slave threads
      - Introduce (pre|post)_cluster_restore handlers to execute a
        mysqlmaster.py switch after restoring all snapshots
      - Fix some PEP8 errors
      - Get rid of the old style load_drivers() method which required a
        bunch of arguments. Use the Znappy method instead

--- znappy/command/cluster.py
+++ znappy/command/cluster.py
@@ -15,7 +15,6 @@ import getpass
 from datetime import datetime, timedelta
 from prettytable import PrettyTable
 from fabric.api import env, sudo, task
-from fabric.colors import green, red
 
 env.warn_only = True
 
@@ -39,28 +38,27 @@ def list_snapshots(cluster, t):
     return snapshots
 
 
-def snapshot_table(snapshots, t = int(time.time())):
+def snapshot_table(snapshots, t=int(time.time())):
     table = PrettyTable(fields=['host', 'snapshot', 'time', 'lag'])
-    
     for host in snapshots:
         snapshot = snapshots[host]
-        lag      = int(t-snapshot.time)
+        lag      = int(t - snapshot.time)
 
         table.add_row([host, snapshot.name, snapshot.time, lag])
 
     return table
 
 
-def action_list(cluster, t):
-    snapshots = list_snapshots(cluster, t)
+def action_list(znappy, t):
+    snapshots = list_snapshots(znappy.cluster, t)
 
     table = snapshot_table(snapshots, t)
 
     return table.get_string(sortby='host')
 
 
-def action_restore(cluster, t):
-    snapshots = list_snapshots(cluster, t)
+def action_restore(znappy, t):
+    snapshots = list_snapshots(znappy.cluster, t)
 
     print snapshot_table(snapshots, t).get_string(sortby='host')
 
@@ -79,36 +77,39 @@ def action_restore(cluster, t):
         return 'Failed to verify credentials'
 
     # lock the whole cluster
-    while not cluster.lock():
+    while not znappy.cluster.lock():
         time.sleep(0.5)
 
     # time to bork the sjit
     try:
+        znappy.load_drivers()
+        znappy.execute_event(['pre_cluster_restore'])
         for snapshot in snapshots:
             env.host_string = snapshot.host
-            sudo('znappy snapshot restore {0} --cluster={1}'.format(snapshot.name, cluster.name))
+            sudo('znappy snapshot restore {0}'.format(snapshot.name))
+        znappy.execute_event(['post_cluster_restore'], master_host=master)
     # ye it should be that easy
     except Exception, e:
         print e.message
         return "Failed to restore cluster! :S"
 
-    cluster.release()
+    znappy.cluster.release()
 
     return "cluster restore complete"
 
 
 def main(db, args):
-    module= sys.modules[__name__]
+    module = sys.modules[__name__]
     znappy = Znappy(db, args['--cluster'])
 
     time_before = datetime.today() - timedelta(minutes=int(args['--minute']))
     time_before = int(time.mktime(time_before.timetuple()))
-    result  = "Action not found!"
+    result      = "Action not found!"
 
     for c in ['list', 'restore']:
         funcname = 'action_{}'.format(c)
         if args[c] and hasattr(module, funcname):
             action = getattr(module, funcname)
-            result = action(znappy.cluster, time_before)
+            result = action(znappy, time_before)
 
     print result
