cdf704a96f9d384a12b765ef81c1b98dec10cbdd
Jorn Wijnands <jwijnands@ebay.com>
2016-01-29 12:27:21 +0100

    Fix infite loop on expired session
      The renew will now try to start a new session, if this fails it will
      exit(1), this will cause upstart to start a new instance.
    
      Fixes #36

--- znappy/keyvalue.py
+++ znappy/keyvalue.py
@@ -1,6 +1,7 @@
 import consul
 import logging
 import time
+import sys
 
 logger = logging.getLogger(__name__)
 
@@ -18,6 +19,7 @@ class KeyValue(object):
         self.close()
 
     def connect(self):
+        logger.info('Connecting to consul and starting new session')
         self._session_id = self._consul.session.create(name="znappy-agent", ttl=30, lock_delay=0)
 
         logger.debug("Connected to Consul: {}".format(self._session_id))
@@ -48,7 +50,20 @@ class KeyValue(object):
         while True:
             try:
                 return self._consul.session.renew(self._session_id)
-            except:
+            except consul.base.NotFound:
+                # session expired/not found
+                logger.warn('Failed to renew session {}, the session is probably expired'.format(self._session_id))
+
+                # try to start a new session, otherwise give up
+                if not self.connect():
+                    # exit with exit code 1, so that upstart will try to restart the service with a clean start
+                    logger.fatal('Failed to start new session :( giving up')
+                    sys.exit(1)
+            except Exception, e:
+                # this error may be recoverable, will try a couple of times
+                # this loop is not really infite, it will, at some point, get a session not found exception
+                # and will hit the except above
+                logger.warn('Failed to renew session {} with error {}, will keep trying'.format(self._session_id, e.message))
                 time.sleep(1)
 
     def get(self, *args, **kwargs):
@@ -59,6 +74,7 @@ class KeyValue(object):
             try:
                 return self._consul.kv.put(*args, dc=self.dc, **kwargs)
             except Exception:
+                logger.warn('Failed to update key {}'.format(args[0]))
                 time.sleep(1)
 
     def delete(self, *args, **kwargs):
@@ -67,8 +83,12 @@ class KeyValue(object):
     def acquire(self, lock, *args, **kwargs):
         logger.debug("trying to acquire lock {0} for {1}".format(lock, self._session_id))
         # TODO implement semaphore
-        result = self._consul.kv.put(lock, "", acquire=self._session_id, dc=self.dc)
-        logger.debug(result)
+        try:
+            result = self._consul.kv.put(lock, "", acquire=self._session_id, dc=self.dc)
+            logger.debug(result)
+        except Exception:
+            # if the put fails for whatever reason, we didn't get it
+            return False
         return result
 
     def release(self, lock, *args, **kwargs):
